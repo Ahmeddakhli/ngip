20c9b2e8797c02d4cfaca6cf46c2f08201634120
Ahmed Dakhli <ahmed.dakhli99@8worx.com>
2023-03-19 11:57:52 +0200

    edit project form

--- resources/views/front/pages/single-project.blade.php
+++ resources/views/front/pages/single-project.blade.php
@@ -836,13 +836,28 @@ array_push($images, $attch->url);
                 </div>
                 {{-- @endif --}}
                 <div class="ask">
-                    <h5>Interested! Ask about this unit</h5>
-                    <form>
-                        <input type="text" required placeholder="Your Name" id='focus-ask' />
-                        <input type="text" required placeholder="Your Mobile" />
-                        <input type="email" required placeholder="Your Email" />
-                        <textarea placeholder="Your Message" required rows="7"></textarea>
-                        <button>SUBMIT</button>
+                    <h5>{{ __('main.contact_us') }}</h5>
+                    <form action="{{ route('contact_us.contact_us.store') }}" method="POST" class="form-contact"
+                        data-parsley-validate>
+                        @csrf
+                        <input type="hidden" name="link" value="{{ Request::url() }}">
+                        <input type="hidden" name="type" value="set_a_visit">
+                        <input type="hidden" name="position" value="{{ isset($position) ? $position : null }}">
+                        <input type="hidden" name="city_id" value="{{ isset($city_id) ? $city_id : null }}">
+                        <input type="hidden" name="model_name" value="{{ isset($model_name) ? $model_name : null }}">
+                        <input type="text" class="form-control" inputmode="text" name="full_name"
+                            placeholder="{{ __('users.full_name') }}" data-parsley-trigger="change focusout" required
+                            data-parsley-required-message="{{ __('main.please_enter_your_name') }}">
+                        <input type="email" class="form-control" inputmode="email" name="email"
+                            placeholder="{{ __('users.email') }}" required
+                            data-parsley-required-message="{{ __('main.please_enter_your_email') }}">
+                        <input type="text" class="form-control" inputmode="tel"
+                            placeholder="{{ __('users.mobile_number') }}" name="phone"
+                            data-parsley-trigger="change focusout" required
+                            data-parsley-required-message="{{ __('main.please_enter_your_mobile_number') }}">
+                        <textarea class="form-control" inputmode="text" placeholder="{{ __('contactus::contact_us.message') }}" name="message"
+                            data-parsley-trigger="change focusout"></textarea>
+                        <button type="submit" class="site-btn contact-from">{{ __('main.send') }}</button>
                     </form>
                 </div>
             </div> <!-- close flex-box -->
